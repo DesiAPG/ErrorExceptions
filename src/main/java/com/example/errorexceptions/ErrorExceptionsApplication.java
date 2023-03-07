ff29a24c3b29bcc59fe647d5b7db87b4e98bf292
DesiAPG <74121435+DesiAPG@users.noreply.github.com>
2023-03-06 21:42:17 -0500

    First Commit

---
+++ src/main/java/com/example/errorexceptions/ErrorExceptionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.errorexceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorExceptionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErrorExceptionsApplication.class, args);
+    }
+
+}
